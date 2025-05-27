2a718eddc910b63db76e35c4021e089b630ebe12
teja-ghankot <saiteja.ghankot17@gmail.com>
2025-05-27 19:25:21 +0530

    backed4

--- auth.js
+++ auth.js
@@ -87,7 +87,7 @@ app.post('/login', async (req, res) => {
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(401).json({ message: 'Invalid credentials.' });
     const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ message: 'Login successful', token, role: user.role ,  userId: user._id });
+    res.status(200).json({ message: 'Login successful', token, role: user.role ,  userId: user._id , phonenumber: mobileNumber});
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
